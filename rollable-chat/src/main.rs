edce5abe9291dc4bcd0ee6cbd63a3d59e422b198
Hopper <daniel.biasatti@gmail.com>
2022-06-02 23:18:31 -0500

    Warp tutorial while I figure out which web server/ websocket lib I want

---
+++ rollable-chat/src/main.rs
@@ -0,0 +1,22 @@
+use warp::Filter;
+
+const WEB_FOLDER: &str = "web-folder/";
+
+#[tokio::main]
+async fn main() {
+    // APIs
+    let hi = warp::path("hi").and(warp::get()).map(|| "Hello from hi");
+    let apis = hi;
+
+    // Static Content
+    let content = warp::fs::dir(WEB_FOLDER);
+    let root = warp::get()
+        .and(warp::path::end())
+        .and(warp::fs::file(format!("{}/index.html", WEB_FOLDER)));
+    let static_site = content.or(root);
+
+    let routes = apis.or(static_site);
+
+    println!("start web-server");
+    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
+}
